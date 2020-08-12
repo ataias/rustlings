8de2c69c7db25a6f3db7e9d42e203d21ba0353d7
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-08-12 07:57:00 -0300

    Answer some exercises

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -4,14 +4,14 @@
 // This store is having a sale where if the price is an even number, you get
 // 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
 
-// I AM NOT DONE
+type int = i64;
 
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: int) -> int {
     if is_even(price) {
         price - 10
     } else {
@@ -19,6 +19,6 @@ fn sale_price(price: i32) -> {
     }
 }
 
-fn is_even(num: i32) -> bool {
+fn is_even(num: int) -> bool {
     num % 2 == 0
 }
